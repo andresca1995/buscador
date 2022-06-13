96e76707de6e72e9c6fabdbc888ea5fa3fcd7f4f
aescobar <aescobar@grupodot.com>
2022-06-13 17:58:57 -0500

    ajuste bug negrilla

--- js/app.js
+++ js/app.js
@@ -70,11 +70,21 @@ function buscartext($event) {
                     let html = "";
 
                     list.forEach(element => {
-                         let bold = element.view.toUpperCase()
-                         let text = bold.split(texto_buscar.toUpperCase());
-                         let antes  = text[0].toLowerCase();
-                         let depues  = text[1]?text[1].toLowerCase():""
-                        html = html + "<a href=" + element.url + "><li >" +antes+"<b>"+texto_buscar+"</b>"+depues+ "</li></a>";
+                         let bold = element.view.toLowerCase()
+                         let text = bold.split(texto_buscar.toLowerCase());
+                         let pa = ""
+                         let a = 0
+                         for (let it = 0; it < text.length; it++) {
+                            const element = text[it];
+                            if(it+1 == text.length){
+                                pa = pa+""+text[it]
+                            }else{
+                                pa = pa+""+text[it]+"<b>"+texto_buscar+"</b>"
+                            }
+                         }
+
+                         console.log(pa)
+                         html = html + "<a href=" + element.url + "><li>"+pa+"</li></a>";
                     });
                     document.getElementById("autocom_box").innerHTML = html
                 }else{
