54513a032b8e7a140a6a44c8713337ca520105fc
murrego <murrego@grupodot.com>
2022-06-17 11:14:20 -0500

    Ultimos ajustes para implementacion en sitio web

--- js/style.js
+++ js/style.js
@@ -4,12 +4,8 @@ let inputBox = searchWrapper.querySelector('input');
 let suggBox = searchWrapper.querySelector('.autocom_box');
 
 $(document).ready(function() {
-  /* var contentLastMarginLeft = 0; */
-  $(".active").click(function() {
+  $(".searcher").click(function() {
      $(".container_all").toggle();
      document.getElementById("texto").focus();
-   /*  box.animate({
-      "margin-left": newValue
-    }, 2000); */
   });
 });
